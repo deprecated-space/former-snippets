5d8d199ad4b7ebf8f1730e6cff09dd4c82dcf94f
hanzichi <hanzichi@outlook.com>
2015-09-21 15:07:51 +0800

    add

---
+++ template/cpp_head_file.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <queue>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <numeric>
+#include <functional>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const ll Inf = ~(1ll << 63);
+const int inf = 0x7fffffff;
+const int N = 50005;
+const double PI = acos(-1.0);
+const double eps = 1e-8;
+#define CC(a,b) memset(a,b,sizeof(a))
+template<class Z>inline bool checkmax(Z &a,Z b){if(a < b || a == -1) {a = b; return true;} return false;}
+template<class Z>inline bool checkmin(Z &a,Z b){if(a > b || a == -1) {a = b; return true;} return false;}
+template<class Z>inline Z ABS(Z a) {return a > 0 ? a : -a;}
+int cas = 0; void printcas() {printf("Case %d: ", ++cas);}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main() {
+  return 0;
+}
