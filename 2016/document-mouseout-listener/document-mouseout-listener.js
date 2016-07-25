0a60c2bb3f112a81fec8eee27867e6d085e0767a
hanzichi <hanzichi@outlook.com>
2016-07-25 09:43:52 +0800

    add document-mouseout-listener.js

---
+++ 2016/document-mouseout-listener/document-mouseout-listener.js
@@ -0,0 +1,25 @@
+// 一个网页嵌套了一个 iframe
+// 我们需要监听鼠标移除该 iframe 的事件
+// @see http://stackoverflow.com/questions/923299/how-can-i-detect-when-the-mouse-leaves-the-window
+
+
+function addEvent(obj, evt, fn) {
+    if (obj.addEventListener) {
+        obj.addEventListener(evt, fn, false);
+    }
+    else if (obj.attachEvent) {
+        obj.attachEvent("on" + evt, fn);
+    }
+}
+
+addEvent(window,"load",function(e) {
+    addEvent(document, "mouseout", function(e) {
+        e = e ? e : window.event;
+        var from = e.relatedTarget || e.toElement;
+        if (!from || from.nodeName == "HTML") {
+            // stop your drag event here
+            // for now we can just use an alert
+            alert("left window");
+        }
+    });
+});
